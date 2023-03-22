ab44623fa30582f60b65943630f5e3207ecd2c45
Dragoslav Ivkovic <Ivkovic.dragoslav.dragi@gmail.com>
2023-03-22 15:09:56 +0100

    task: drawer right setUp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect, useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -6,9 +7,66 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Box } from '@mui/material';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { styled, useTheme } from '@mui/material/styles';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import getUsers from '../components/utils/api';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginRight: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-start',
+}));
 
 function Home() {
   const [userList, setUserList] = useState([]);
@@ -41,61 +99,113 @@ function Home() {
     setSortByTitle(sortByTitle === 'asc' ? 'desc' : 'asc');
   };
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const theme = useTheme();
   return (
-    <div>
-      <button onClick={sortByAuthorFn} type="button">
-        sortByAuthorFn
-      </button>
-      <button onClick={setSortByTitleFn} type="button">
-        setSortByTitleFn
-      </button>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <TableContainer component={Paper} sx={{ width: '500px' }}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="right" />
-                <TableCell align="right">Title</TableCell>
-                <TableCell align="right">Author</TableCell>
-                <TableCell align="right">Year</TableCell>
-                <TableCell align="right">Pages</TableCell>
-                <TableCell align="right">Quantity</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {userList.map((row) => (
-                <TableRow
-                  key={row.title}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell align="right">
-                    <Box
-                      component="img"
-                      src={row.coverPhoto}
-                      alt={row.title}
-                      sx={{
-                        height: 100,
-                        width: 70,
-                      }}
-                    />
-                  </TableCell>
-                  <TableCell align="right">{row.title}</TableCell>
-                  <TableCell align="right">{row.nameOfAuthor}</TableCell>
-                  <TableCell align="right">{row.yearOfBublishing}</TableCell>
-                  <TableCell align="right">{row.numOfPages}</TableCell>
-                  <TableCell align="right">{row.quantity}</TableCell>
-                  <TableCell align="right">
-                    <MoreVertIcon />
-                  </TableCell>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
+            Persistent drawer
+          </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={{ ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Main open={open}>
+        <DrawerHeader />
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <TableContainer component={Paper}>
+            <div>
+              <button onClick={sortByAuthorFn} type="button">
+                sortByAuthorFn
+              </button>
+              <button onClick={setSortByTitleFn} type="button">
+                setSortByTitleFn
+              </button>
+            </div>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="right" />
+                  <TableCell align="right">Title</TableCell>
+                  <TableCell align="right">Author</TableCell>
+                  <TableCell align="right">Year</TableCell>
+                  <TableCell align="right">Pages</TableCell>
+                  <TableCell align="right">Quantity</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
-    </div>
+              </TableHead>
+              <TableBody>
+                {userList.map((row) => (
+                  <TableRow
+                    key={row.title}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align="right">
+                      <Box
+                        component="img"
+                        src={row.coverPhoto}
+                        alt={row.title}
+                        sx={{
+                          height: 100,
+                          width: 70,
+                        }}
+                      />
+                    </TableCell>
+                    <TableCell align="right">{row.title}</TableCell>
+                    <TableCell align="right">{row.nameOfAuthor}</TableCell>
+                    <TableCell align="right">{row.yearOfBublishing}</TableCell>
+                    <TableCell align="right">{row.numOfPages}</TableCell>
+                    <TableCell align="right">{row.quantity}</TableCell>
+                    <TableCell align="right">
+                      <MoreVertIcon />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+      </Main>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <Divider />
+      </Drawer>
+    </Box>
   );
 }
 
